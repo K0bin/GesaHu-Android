a697ed7ea167c15e31b2d059adf85c077f06b71a
Robin <robin.kertels@outlook.com>
2016-08-30 23:05:09 +0200

    Replace the intro by a new one using AppIntro

--- mobile/src/main/java/rhedox/gesahuvertretungsplan/ui/activity/WelcomeActivity.java
+++ mobile/src/main/java/rhedox/gesahuvertretungsplan/ui/activity/WelcomeActivity.java
@@ -4,8 +4,10 @@ import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Debug;
+import android.os.PersistableBundle;
 import android.support.annotation.LayoutRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.graphics.drawable.VectorDrawableCompat;
@@ -16,15 +18,19 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.BounceInterpolator;
 
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
 import com.squareup.leakcanary.RefWatcher;
 
 import butterknife.BindView;
@@ -35,250 +41,47 @@ import rhedox.gesahuvertretungsplan.App;
 import rhedox.gesahuvertretungsplan.BuildConfig;
 import rhedox.gesahuvertretungsplan.R;
 import rhedox.gesahuvertretungsplan.ui.fragment.PreferenceFragment;
+import rhedox.gesahuvertretungsplan.ui.fragment.WelcomePreferenceFragment;
 
-public class WelcomeActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+public class WelcomeActivity extends AppIntro {
 
-    private PagerAdapter pagerAdapter;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+		super.onCreate(savedInstanceState);
 
-    @BindView(R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.viewPager) ViewPager viewPager;
-    @BindView(R.id.fab) FloatingActionButton floatingActionButton;
-    private Unbinder unbinder;
+		addSlide(AppIntroFragment.newInstance(getString(R.string.feature_design), getString(R.string.feature_design_description), R.drawable.ic_view_list, ContextCompat.getColor(this, R.color.intro_slide_1)));
+		addSlide(AppIntroFragment.newInstance(getString(R.string.feature_highlight), getString(R.string.feature_highlight_description), R.drawable.ic_invert_colors, ContextCompat.getColor(this, R.color.intro_slide_2)));
+		addSlide(AppIntroFragment.newInstance(getString(R.string.feature_notify), getString(R.string.feature_notify_description), R.drawable.ic_bell, ContextCompat.getColor(this, R.color.intro_slide_3)));
+		addSlide(WelcomePreferenceFragment.newInstance());
 
-    private ObjectAnimator animator;
+		setBarColor(Color.parseColor("#1c000000"));
+		setSeparatorColor(Color.parseColor("#1c000000"));
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.GesahuTheme);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+		// Check if the version of Android is Lollipop or higher
+		if (Build.VERSION.SDK_INT >= 21) {
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+					WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+		}
+	}
 
-        unbinder = ButterKnife.bind(this);
+	@Override
+	public void onDonePressed(Fragment currentFragment) {
+		super.onDonePressed(currentFragment);
 
-        setSupportActionBar(toolbar);
+		loadMainActivity();
+	}
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+	private void loadMainActivity() {
 
-        // Set up the ViewPager with the sections adapter.
-        viewPager.setAdapter(pagerAdapter);
-        viewPager.addOnPageChangeListener(this);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putBoolean(PreferenceFragment.PREF_PREVIOUSLY_STARTED, true);
+		editor.apply();
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-    }
+		Intent intent = new Intent(this, MainActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+		startActivity(intent);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-
-        animator = ObjectAnimator.ofFloat(new PagerHintMovement(10), "progress", -1f, 1f);
-        animator.setInterpolator(new BounceInterpolator());
-        animator.setDuration(2000);
-        animator.setRepeatCount(1);
-        animator.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {
-                viewPager.beginFakeDrag();
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                if(viewPager != null && viewPager.isFakeDragging())
-                    viewPager.endFakeDrag();
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-                if(viewPager != null) {
-                    if(viewPager.isFakeDragging())
-                        viewPager.endFakeDrag();
-
-                    viewPager.setCurrentItem(0);
-                }
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-
-            }
-        });
-        animator.setStartDelay(500);
-        animator.start();
-    }
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-
-        if(position == pagerAdapter.getCount() - 2)
-            floatingActionButton.setImageResource(R.drawable.ic_check);
-        else if(position == pagerAdapter.getCount() - 1)
-            nextPage(null);
-        else
-            floatingActionButton.setImageResource(R.drawable.ic_forward);
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-
-    }
-
-    @OnClick(R.id.fab)
-    public void nextPage(View view) {
-        try {
-            if (viewPager != null && viewPager.isFakeDragging())
-                viewPager.endFakeDrag();
-
-            if (animator != null)
-                animator.cancel();
-        }
-        catch(NullPointerException e) {
-            Log.e("Workaround", String.format("Android bug workaround in nextPage, Exception: %s",e.getMessage()));
-        }
-
-        if(viewPager != null && viewPager.getCurrentItem() < pagerAdapter.getCount() - 2)
-            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
-        else {
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-            SharedPreferences.Editor edit = prefs.edit();
-            edit.putBoolean(PreferenceFragment.PREF_PREVIOUSLY_STARTED, true);
-            edit.apply();
-
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        if(animator != null)
-            animator.cancel();
-
-        unbinder.unbind();
-
-        //LeakCanary
-        RefWatcher refWatcher = App.getRefWatcher(this);
-        if(refWatcher != null && pagerAdapter != null)
-            refWatcher.watch(pagerAdapter);
-    }
-
-    class PagerHintMovement {
-
-        float goal;
-        float progress;
-
-        PagerHintMovement(float goal) {
-            this.goal = goal;
-        }
-
-        public float getProgress() {
-            return progress;
-        }
-
-        public void setProgress(float progress) {
-            this.progress = progress;
-
-            if(viewPager != null && viewPager.isFakeDragging()) {
-
-                if(BuildConfig.DEBUG)
-                    Log.d("ViewPager Hint", "Animation: Updating fake drag with value=" + goal * progress + "px");
-
-                try {
-                    viewPager.fakeDragBy(goal * progress);
-                } catch (NullPointerException e) {
-
-                }
-            }
-
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public static class PagerAdapter extends FragmentPagerAdapter {
-
-        public PagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-
-            switch (position) {
-                case 0:
-                    return IntroductionFragment.newInstance(R.layout.fragment_welcome);
-
-                case 1:
-                    return IntroductionFragment.newInstance(R.layout.fragment_notify);
-
-                case 2:
-                    return PreferenceFragment.newInstance();
-
-                case 3:
-                    return new Fragment();
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return 4;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return null;
-        }
-    }
-
-    public static class IntroductionFragment extends Fragment {
-
-        @LayoutRes
-        private int layoutId = -1;
-
-        public static final String ARG_LAYOUT = "arg_layout";
-
-        public static IntroductionFragment newInstance(@LayoutRes int layoutId) {
-            IntroductionFragment fragment = new IntroductionFragment();
-            Bundle bundle = new Bundle(1);
-            bundle.putInt(ARG_LAYOUT, layoutId);
-            fragment.setArguments(bundle);
-            return fragment;
-        }
-
-        public IntroductionFragment() {
-        }
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            if(getArguments() != null && getArguments().containsKey(ARG_LAYOUT))
-                layoutId = getArguments().getInt(ARG_LAYOUT);
-
-            View rootView = inflater.inflate(layoutId != -1 ? layoutId : R.layout.fragment_welcome, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onDestroy() {
-            //LeakCanary
-            RefWatcher refWatcher = App.getRefWatcher(getActivity());
-            if(refWatcher != null)
-                refWatcher.watch(this);
-
-            super.onDestroy();
-        }
-    }
+	}
 }
