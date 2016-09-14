3a7dae5aa0c799ff53b0c78926f98749906831c1
Robin <robin.kertels@outlook.com>
2016-09-14 23:36:59 +0200

    Replace icons on intro screen

--- mobile/src/main/java/rhedox/gesahuvertretungsplan/ui/activity/WelcomeActivity.java
+++ mobile/src/main/java/rhedox/gesahuvertretungsplan/ui/activity/WelcomeActivity.java
@@ -1,44 +1,19 @@
 package rhedox.gesahuvertretungsplan.ui.activity;
 
-import android.animation.Animator;
-import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
-import android.os.Debug;
-import android.os.PersistableBundle;
-import android.support.annotation.LayoutRes;
-import android.support.design.widget.FloatingActionButton;
-import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.preference.PreferenceManager;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.view.WindowManager;
-import android.view.animation.BounceInterpolator;
 
 import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroFragment;
-import com.squareup.leakcanary.RefWatcher;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Unbinder;
-import rhedox.gesahuvertretungsplan.App;
-import rhedox.gesahuvertretungsplan.BuildConfig;
 import rhedox.gesahuvertretungsplan.R;
 import rhedox.gesahuvertretungsplan.ui.fragment.PreferenceFragment;
 import rhedox.gesahuvertretungsplan.ui.fragment.WelcomePreferenceFragment;
@@ -50,8 +25,8 @@ public class WelcomeActivity extends AppIntro {
 		getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 		super.onCreate(savedInstanceState);
 
-		addSlide(AppIntroFragment.newInstance(getString(R.string.feature_design), getString(R.string.feature_design_description), R.drawable.ic_view_list, ContextCompat.getColor(this, R.color.intro_slide_1)));
-		addSlide(AppIntroFragment.newInstance(getString(R.string.feature_highlight), getString(R.string.feature_highlight_description), R.drawable.ic_invert_colors, ContextCompat.getColor(this, R.color.intro_slide_2)));
+		addSlide(AppIntroFragment.newInstance(getString(R.string.feature_design), getString(R.string.feature_design_description), R.drawable.ic_phone, ContextCompat.getColor(this, R.color.intro_slide_1)));
+		addSlide(AppIntroFragment.newInstance(getString(R.string.feature_highlight), getString(R.string.feature_highlight_description), R.drawable.ic_relevant, ContextCompat.getColor(this, R.color.intro_slide_2)));
 		addSlide(AppIntroFragment.newInstance(getString(R.string.feature_notify), getString(R.string.feature_notify_description), R.drawable.ic_bell, ContextCompat.getColor(this, R.color.intro_slide_3)));
 		addSlide(WelcomePreferenceFragment.newInstance());
 
