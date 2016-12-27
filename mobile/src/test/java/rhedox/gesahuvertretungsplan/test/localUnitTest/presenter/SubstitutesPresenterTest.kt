dc8b96e3ca8382543ba3582cc039c310c6a9d817
Robin <robin.kertels@outlook.com>
2016-12-27 01:45:10 +0100

    Import Mockito-Kotlin

--- mobile/src/test/java/rhedox/gesahuvertretungsplan/test/localUnitTest/presenter/SubstitutesPresenterTest.kt
+++ mobile/src/test/java/rhedox/gesahuvertretungsplan/test/localUnitTest/presenter/SubstitutesPresenterTest.kt
@@ -2,31 +2,32 @@ package rhedox.gesahuvertretungsplan.test.localUnitTest.presenter
 
 import android.content.ContentResolver
 import android.os.Bundle
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.instance
+import com.nhaarman.mockito_kotlin.mock
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.powermock.api.mockito.PowerMockito
-import org.powermock.api.mockito.PowerMockito.`when`
-import org.powermock.core.classloader.annotations.PowerMockIgnore
-import org.powermock.core.classloader.annotations.PrepareEverythingForTest
-import org.powermock.core.classloader.annotations.PrepareForTest
-import org.powermock.modules.junit4.PowerMockRunner
-import org.powermock.modules.junit4.PowerMockRunnerDelegate
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.RuntimeEnvironment
-import org.robolectric.annotation.Config
 import rhedox.gesahuvertretungsplan.BuildConfig
 import rhedox.gesahuvertretungsplan.presenter.SubstitutesPresenter
 
 /**
  * Created by robin on 22.12.2016.
  */
-@RunWith(RobolectricTestRunner::class)
-@Config(constants = BuildConfig::class, sdk = intArrayOf(21))
+//@RunWith(RobolectricTestRunner::class)
+//@Config(constants = BuildConfig::class, sdk = intArrayOf(21))
 //@RunWith(PowerMockRunner::class)
-@PowerMockRunnerDelegate(RobolectricTestRunner::class)
-@PowerMockIgnore("org.mockito.*", "org.robolectric.*", "android.*")
-@PrepareForTest(ContentResolver::class)
+//@PowerMockRunnerDelegate(RobolectricTestRunner::class)
+//@PowerMockIgnore("org.mockito.*", "org.robolectric.*", "android.*")
+//@PrepareForTest(ContentResolver::class)
 class SubstitutesPresenterTest {
+    val kodein = Kodein {
+        bind<SubstitutesPresenter>() with instance(
+            mock<SubstitutesPresenter> {
+
+            }
+        )
+    }
+
     @Test
     fun test() {
         //PowerMockito.mockStatic(ContentResolver::class.java);
